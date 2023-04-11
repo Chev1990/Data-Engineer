70831b252f49a4514fe78de8e96f7b692c2f10ba
Ekaterina_Chudinova <katerinachev1@gamil.com>
2023-04-11 19:01:22 +0200

    pytest

--- pytest_data/venv/test_data.py
+++ pytest_data/venv/test_data.py
@@ -17,21 +17,17 @@ def connectdb():
 """
 @pytest.mark.parametrize('tablename', ["jobs", "employees"])
 def testempty(tablename):
-    conn = connectdb()
-    cur = conn.cursor()
+    cur = connectdb().cursor()
     query = f"Select count(*) cnt from hr.{tablename}"
     cur.execute(query)
     row = cur.fetchall()
-    conn.close()
-    assert row[0][0] > 0, f'Table {tablename} is empty'
-    #if row[0][0] > 50:
-     #  print(f'Table {tablename} is not empty')
-      # conn.close()
-      # return 1
-    #else:
-     #   conn.close()
-      #  str=f"Table {tablename} is empty"
-      #  raise Exception(str)
+    #assert row[0][0] > 0, f'Table {tablename} is empty'
+    if row[0][0] > 0:
+       print(f'Table {tablename} is not empty')
+       return 1
+    else:
+        print(f'Table {tablename} is empty')
+        return 0
 """
     AUTOTEST-002: [employees] validity
     Check for average salary of employees 
@@ -41,13 +37,17 @@ def testempty(tablename):
     Expected result: the average salary of employees = 8060, status: pass
 """
 def testavgsalary():
-    conn = connectdb()
-    cur = conn.cursor()
+    cur = connectdb().cursor()
     query = f"select avg(salary) from hr.employees"
     cur.execute(query)
     row = cur.fetchall()
-    conn.close()
-    assert row[0][0] == 8060, f'The average salary is not equal to 8060'
+    #assert row[0][0] == 8060, f'The average salary is not equal to 8060'
+    if row[0][0] == 8060:
+       print(f'The average salary is equal to 8060')
+       return 1
+    else:
+        print(f'The average salary is not equal to 8060')
+        return 0
 """
     AUTOTEST-003: [departments] completeness
     Check for count the number of departments in table(departments)
@@ -57,13 +57,17 @@ def testavgsalary():
     Expected result: the number of distinct department_name = 11, status: pass
 """
 def testcntkdepartments():
-    conn = connectdb()
-    cur = conn.cursor()
+    cur = connectdb().cursor()
     query = f"select count(distinct department_name) from [hr].[departments]"
     cur.execute(query)
     row = cur.fetchall()
-    conn.close()
-    assert row[0][0] == 11, f'the number of distinct department_name is not equal to 11'
+    #assert row[0][0] == 11, f'the number of distinct department_name is not equal to 11'
+    if row[0][0] == 11:
+       print(f'the number of distinct department_name is equal to 11')
+       return 1
+    else:
+        print(f'the number of distinct department_name is not equal to 11')
+        return 0
 """
     AUTOTEST-004: [jobs] uniqueness
     Check for duplicate rows in table jobs
@@ -73,13 +77,17 @@ def testcntkdepartments():
     Expected result: the number of duplicate rows equals 0, status: pass
 """
 def testuniquenessjobs():
-    conn = connectdb()
-    cur = conn.cursor()
+    cur = connectdb().cursor()
     query = f"select  job_id, job_title, min_salary,max_salary, count(*) cnt from hr.jobs group by job_id, job_title, min_salary,max_salary having count(*)>1"
     cur.execute(query)
     row = cur.fetchall()
-    conn.close()
-    assert len(row) == 0, f'there are the duplicate rows in jobs table'
+    #assert len(row) == 0, f'there are the duplicate rows in jobs table'
+    if len(row) == 0:
+       print(f'there are not the duplicate rows in jobs table')
+       return 1
+    else:
+        print(f'there are the duplicate rows in jobs table')
+        return 0
 """
     AUTOTEST-005: [jobs] accuracy
     Check column value (min_salary) for specified job_title in Table Jobs
@@ -94,13 +102,17 @@ def testuniquenessjobs():
 @pytest.mark.parametrize('jobtitle', ["Public Accountant"])
 @pytest.mark.parametrize('minsalary', [4200])
 def testminsalaryforjobtitle(jobtitle, minsalary):
-    conn = connectdb()
-    cur = conn.cursor()
+    cur = connectdb().cursor()
     query = f"select min_salary from hr.jobs where job_title = '{jobtitle}'"
     cur.execute(query)
     row = cur.fetchall()
-    conn.close()
-    assert row[0][0] == minsalary, f'min_salary is not equal to {minsalary} for job_title = {jobtitle} '
+    #assert row[0][0] == minsalary, f'min_salary is not equal to {minsalary} for job_title = {jobtitle} '
+    if row[0][0] == minsalary:
+       print(f'min_salary is equal to {minsalary} for job_title = {jobtitle} ')
+       return 1
+    else:
+        print(f'min_salary is not equal to {minsalary} for job_title = {jobtitle} ')
+        return 0
 
 """
     AUTOTEST-006: [employees] validity
@@ -112,19 +124,23 @@ def testminsalaryforjobtitle(jobtitle, minsalary):
 """
 
 def testmaxsalaryofemployees():
-    conn = connectdb()
-    cur = conn.cursor()
+    cur = connectdb().cursor()
     query = f"select max(salary) from hr.employees"
     cur.execute(query)
     row = cur.fetchall()
-    conn.close()
-    assert row[0][0] == 24000, f'max_salary of employees is not equal to 24000'
+    #assert row[0][0] == 24000, f'max_salary of employees is not equal to 24000'
+    if row[0][0] == 24000:
+       print(f'max_salary of employees is equal to 24000')
+       return 1
+    else:
+        print(f'max_salary of employees is not equal to 24000')
+        return 0
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
    # testemployees('employees')
     testempty('jobs')
     testavgsalary()
     testcntkdepartments()
     testuniquenessjobs()
     testminsalaryforjobtitle('Public Accountant', 4200)
-    testmaxsalaryofemployees()'''
+    testmaxsalaryofemployees()
