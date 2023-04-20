727a8d72d8cbd9ffa553dc5940267ca8e0545687
Ekaterina_Chudinova <katerinachev1@gamil.com>
2023-04-20 19:04:20 +0200

    new connect

--- pytest_data/venv/test_data.py
+++ pytest_data/venv/test_data.py
@@ -5,7 +5,7 @@ import math
 
 def connectdb():
     #conndb = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};Server=(localdb)\\MyInstance;Database=TRN;integrated security=true')
-    conndb = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};UID=TestDQA;PWD=Password12345;Database=TRN;Server=np:\\.\pipe\LOCALDB#25DBD486\tsql\query;')
+    conndb = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};UID=TestDQA;PWD=Password12345;Database=TRN;Server=tcp:127.0.0.1;')
     #conndb = pymssql.connect(host='127.0.0.1',user='TestDQA',password="Password12345",database='TRN')#(localdb)\\MyInstance
     return conndb
 """
